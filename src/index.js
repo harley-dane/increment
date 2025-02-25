3eb3a7a3980c4958ba733d719cd68a38718acc88
Harley Clair <60947601+harley-dane@users.noreply.github.com>
2025-02-25 10:17:09 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('Hello, world!');
+// dom.js
+const dom =  {
+    count : document.querySelector(".count"),
+    increment : document.getElementById("increment"),
+    reset : document.getElementById("reset"),
+    decrement : document.getElementById("decrement")
+}
+
+//data
+const data = {
+    count: 0
+}
+
+// Increment function
+const handelIncrement = () => {
+    data.count += 1
+    dom.count.innerText = data.count
+    localStorage.setItem(data.count, dom.count)
+}
+
+// Decrement function
+const handelDecrement = () => { 
+    data.count -= 1
+    dom.count.innerText = data.count
+}
+
+// Reset function
+const handelReset = () => { 
+    data.count = 0
+    dom.count.innerText = data.count
+    
+}
+
+// Event Listeners
+dom.increment.addEventListener("click", handelIncrement)
+dom.reset.addEventListener("click", handelReset)
+dom.decrement.addEventListener("click", handelDecrement)
